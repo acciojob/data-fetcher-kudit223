27104425c3e5f03b82e5d61568956ea397567b6a
Udit Kumar <kudit223@gmail.com>
2026-03-05 00:53:54 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 
-import React from "react";
+import React, { useState,useEffect } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  const [displayData,setDisplayData]=useState(null)
+  const [loading,setLoading]=useState(true);
+  useEffect(()=>{
+    fetch('https://dummyjson.com/products').then((response)=>{
+      if(!response.ok){
+        throw new Error(`HTTP Error, Status: ${response.status}`)
+      }
+      return response.json();
+    }).then(data=>{
+      setTimeout(()=>{
+        setDisplayData(data.products);
+        setLoading(false);
+        console.log(data.products)
+      },1000)
+    })
+  },[]);
+
+  if(loading){
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Data Fetched From API</h1>
+       <pre>
+        {JSON.stringify(displayData,null ,2)}
+       </pre>
     </div>
   )
 }
